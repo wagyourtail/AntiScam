71453552220bfe1b9bbbf7db59c37d7fdf5a3cb9
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-11-02 13:28:20 -0600

    trim token in register

--- registercommands.ts
+++ registercommands.ts
@@ -2,7 +2,7 @@ import { REST } from "@discordjs/rest";
 import { SlashCommandBuilder, SlashCommandChannelOption, SlashCommandStringOption } from "@discordjs/builders";
 import { ChannelType, Routes } from "discord-api-types/v9";
 import { readFileSync } from "fs";
-const token = readFileSync("token.txt");
+const token = readFileSync("token.txt").toString().trim();
 const clientid = "902970889178583121";
 const commands = [
     new SlashCommandBuilder().setName("report").setDescription("reports site linked or site linked in message you're replying to as a phishing site.").addStringOption(new SlashCommandStringOption().setName("urls").setDescription("url or list of urls (space seperated)")),
@@ -10,7 +10,7 @@ const commands = [
     new SlashCommandBuilder().setName("invite").setDescription("get a discord invite link")
 ];
 
-const rest = new REST({ version: '9' }).setToken(token.toString());
+const rest = new REST({ version: '9' }).setToken(token);
 rest.put(Routes.applicationCommands(clientid), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
